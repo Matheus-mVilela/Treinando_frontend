64d3f9a16276471c9e20c75fcb2a12dab25482a6
Matheus Vilela <matheus_vilela@icloud.com>
2020-05-08 16:43:32 -0300

    Finalizando Formulario

--- Curso de HTML5/curso-html5-pacote01/projeto-glass-html5/_javascript/funcoes.js
+++ Curso de HTML5/curso-html5-pacote01/projeto-glass-html5/_javascript/funcoes.js
@@ -2,3 +2,9 @@ function mudaFoto(foto) {
     document.getElementById("icone").src = foto;
 }
 
+
+function calc_total(){
+    var quantidade = parseInt(document.getElementById('cqtd'). value);
+    total = quantidade * 1500;
+    document.getElementById('cpreco'). value = total;
+}
